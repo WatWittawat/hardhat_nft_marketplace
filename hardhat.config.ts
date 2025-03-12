45defaa9632362cf180792c909ad9d0b6c9774d3
Wittawat Phongphrit (Wat) <124418134+WatWittawat@users.noreply.github.com>
2025-03-12 14:53:57 +0700

    initialize Hardhat project with basic NFT contract and configuration files

---
+++ hardhat.config.ts
@@ -0,0 +1,37 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-deploy";
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+
+    localhost: {
+      chainId: 31337,
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.7",
+      },
+      {
+        version: "0.4.24",
+      },
+    ],
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+      1: 0, // similarly on mainnet (network 1) it will take the first account as deployer. Note though that depending on how hardhat network are  configured, the account 0 on one network can be different than on another
+    },
+  },
+  mocha: {
+    timeout: 200000, // 200 seconds max for running tests
+  },
+};
+
+export default config;
